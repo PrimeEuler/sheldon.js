ac9a64eff9e9f8db8b2850c16e85b7b6d51de861
Jeremy Ebert <primeeuler@gmail.com>
2018-02-23 10:25:14 -0500

    clean up

--- example/cli/shell.js
+++ example/cli/shell.js
@@ -1,6 +1,30 @@
 var sheldon = require('../../').sheldon
 var shell = new sheldon()
+//SCHEMA
+shell.DEFINITIONS = {
+    'username':{ 'OBJECT-TYPE': 
+                   { SYNTAX: [ 'OCTET STRING', { SIZE: { min: 1, max: 32 } } ],
+                     'MAX-ACCESS': 'read-write',
+                     STATUS: 'current',
+                     DESCRIPTION: 
+                      [ 'A human readable string representing the name of the user.',
+                        'This is the (User-based Security) Model dependent security ID.'],
+                     '::=': [ 'sheldon', 1 ] } },
+    'login':{ 'OBJECT-TYPE': 
+                   { SYNTAX: [ 'function', function(username,password){
+                       
+                       shell.cli.question('username',function(response){
+                           
+                       })
+                   } ],
+                     'MAX-ACCESS': 'execute',
+                     STATUS: 'current',
+                     DESCRIPTION: 
+                      [ 'Request username and password for authentication.',
+                        'This is the (User-based Security) Model dependent security ID.'],
+                     '::=': [ 'sheldon', 1 ] } },
 
+}
 process.stdin.pipe(shell.io).pipe(process.stdout)
 process.stdout.on('resize',function(){
     shell.io.stdout.setSize(process.stdout)
